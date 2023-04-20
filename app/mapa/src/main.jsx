065ffdae78f7650497eb1884379fbad755204bb7
MaritzaB <amby.121192@gmail.com>
2023-04-20 17:08:40 -0600

    🗺️ Deploy map with reac + leaflet ⚛️

--- app/mapa/src/main.jsx
+++ app/mapa/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { MapaLeaflet} from './MapaLeaflet'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MapaLeaflet />
   </React.StrictMode>,
 )
