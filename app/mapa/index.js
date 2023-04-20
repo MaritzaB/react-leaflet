17ba71be4d12ac28329606fc4a2ccd32035360e3
MaritzaB <amby.121192@gmail.com>
2023-04-19 19:35:44 -0600

    🗺️ Add fisrt version of a map

--- app/mapa/index.js
+++ app/mapa/index.js
@@ -1,1 +1,8 @@
 console.log('Camara gente')
+
+var osmTiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+const centroMexicoCity = [19.432608, -99.133209];
+var map = L.map('mapa_base').addLayer(osmTiles).setView(centroMexicoCity, 15);
