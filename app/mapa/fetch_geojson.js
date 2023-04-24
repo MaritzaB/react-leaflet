a79dd5df6b66624b77e0d87bca51e4a8ccce7b79
maritzee <amby.121192@gmail.com>
2023-04-23 22:04:04 -0600

    🎨 Give format

--- app/mapa/fetch_geojson.js
+++ app/mapa/fetch_geojson.js
@@ -1,22 +1,25 @@
 // http://localhost:8081/geoserver/curso_gis/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=curso_gis%3Adenue_inegi_09_&maxFeatures=50&outputFormat=application%2Fjson
 
-const capa = '3Adenue_inegi_09_'
-const maxiFeatures = '2'
+const workingLayer = '3Adenue_inegi_09_'
+const maximumFeatures = '2'
 
 const getServerQuery = (layer, maxFeatures) => {
-    const urlServer  = '127.0.0.1'
+    const urlServer = '127.0.0.1'
     const server = 'geoserver'
     const port = '8081'
     const workspace = 'curso_gis'
     const outputFormat = 'application%2Fjson'
 
-    const query = `http://${urlServer}:${port}/${server}/${workspace}/ows?service=WFS&version=1.0.0&` + 
+    const query = `http://${urlServer}:${port}/${server}/${workspace}/ows?service=WFS&version=1.0.0&` +
         `request=GetFeature&typeName=${workspace}%${layer}&maxFeatures=${maxFeatures}&outputFormat=${outputFormat}`
-    
+
     console.log('El geoJSON es:')
     console.log(query)
     return query
 }
 
-const query = getServerQuery(capa,maxiFeatures)
-fetch(query).then(response => response.json()).then((data) => { console.log(data)})
+const query = getServerQuery(workingLayer, maximumFeatures)
+fetch(query).then(
+    response => response.json()).then(
+        (data) => { console.log(data) }
+    )
