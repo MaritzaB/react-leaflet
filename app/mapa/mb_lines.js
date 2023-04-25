10ffa26f5ae398903ea576ee1a59d6d9d1001438
maritzee <amby.121192@gmail.com>
2023-04-25 00:13:12 -0600

    ⚡️ Show mb lines in osm with leaflet

--- app/mapa/mb_lines.js
+++ app/mapa/mb_lines.js
@@ -1,40 +1,15 @@
 //http://localhost:8081/geoserver/curso_gis/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=curso_gis%3AMetrobus_lineas_utm14n&maxFeatures=50&outputFormat=application%2Fjson
 
 const workingLayer = '3AMetrobus_lineas_utm14n'
-const maximumFeatures = '5'
+const maximumFeatures = '63'
 
 const query = getServerQuery(workingLayer, maximumFeatures)
 
-//var myLineas = [{
-//    "type": "LineString",
-//    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-//}, {
-//    "type": "LineString",
-//    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-//}];
-//L.geoJSON(myLineas).addTo(map);
-
-
 fetch(query).then(
-    response => response.json()).then(
-        (data) => {
-            console.log(data)
-            // Recupera la información para cargar en el mapa
-            data.features.map((element, index) => {
-                const {type} = element.geometry;
-                const cords = [`"coordinates":`, element.geometry.coordinates];
-                console.log({type})
-                console.log(cords)
-                var myLines = [
-                    {type},
-                    cords
-                ]
-                L.geoJSON(myLines).addTo(map);
-            })
+    function (response) {
+        return response.json();
+    }).then(
+        function (data) {
+            L.geoJSON(data).addTo(map);
         }
     )
-
-//L.geoJSON(query).addTo(mapa_base);
-
-
-
