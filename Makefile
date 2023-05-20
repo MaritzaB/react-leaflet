8e8c5b9e70ba025881fef79b0c0537456b6beb15
maritzee <amby.121192@gmail.com>
2023-05-19 18:55:47 -0600

    🚀 Update API query

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
-run:
+up:
 	docker-compose up -d
 	docker-compose exec frontend sh -c "cd mapa && make deploy && bash"
 
 clean:
 	npm prune
-	rm --recursive --force app/mapa/dist
-	rm --recursive --force app/mapa/node_modules
+	sudo rm --recursive --force app/mapa/dist
+	sudo rm --recursive --force app/mapa/node_modules
 
 down:
 	docker-compose down
+	clean
--- app/mapa/getServerQuery.js
+++ app/mapa/getServerQuery.js
@@ -1,14 +1,26 @@
 // Obten el query des servidor
-
-const getServerQuery = (layer, maxFeatures) => {
+// http://localhost:8081/geoserver/curso_gis/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=curso_gis%3AqueryAzcapoData&maxFeatures=30&outputFormat=application%2Fjson&viewparams=
+claveMunicipio:007;claveActividad:465312;minyear:2003;maxyear:2010
+const getServerQuery = (
+        layer, maxFeatures=100, minyear=2003, maxyear=2023, 
+        //claveMunicipio=003,
+        claveActividad=461190
+        ) => {
     const urlServer = '127.0.0.1'
     const server = 'geoserver'
     const port = '8081'
     const workspace = 'curso_gis'
     const outputFormat = 'application%2Fjson'
+    var   viewparams = 'claveMunicipio:' + claveMunicipio 
+                    + ';minyear:' + minyear
+                    + ';maxyear:' + maxyear 
+                    + ';claveActividad:' + claveActividad
 
-    const query = `http://${urlServer}:${port}/${server}/${workspace}/ows?service=WFS&version=1.0.0&` +
-        `request=GetFeature&typeName=${workspace}%${layer}&maxFeatures=${maxFeatures}&outputFormat=${outputFormat}`
+    const query = `http://${urlServer}:${port}/${server}/` +
+        `${workspace}/ows?service=WFS&version=1.0.0&` +
+        `request=GetFeature&typeName=${workspace}%${layer}&` +
+        `maxFeatures=${maxFeatures}&outputFormat=${outputFormat}` +
+        `&viewparams=${viewparams}`
 
     console.log('El geoJSON es:')
     console.log(query)
