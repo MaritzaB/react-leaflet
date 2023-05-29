502219eeb2244cbc0e961b4b1873f5941ac749d3
maritzee <amby.121192@gmail.com>
2023-05-28 23:40:12 -0600

    🚧 Work in progress

--- app/albatross_map/src/App.tsx
+++ app/albatross_map/src/App.tsx
@@ -2,21 +2,26 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css'
 import 'leaflet/dist/leaflet.css';
 
+const GuadalupeIsland = [28.883621, -118.292683];
+const defaultZoom = 9
+
+
 function App() {
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    
+    <MapContainer center={ GuadalupeIsland } zoom={ defaultZoom } >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={ GuadalupeIsland }>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
     </MapContainer>
+    
   )
 }
 
-export default App
