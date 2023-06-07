0f9e32c803264dfcfe79e47947b5c258d2134959
maritzee <amby.121192@gmail.com>
2023-06-07 13:15:30 -0600

    ♻️ Lee arreglo de puntos desde afuera

--- app/albatross_trajectories/src/MapaLeaflet.jsx
+++ app/albatross_trajectories/src/MapaLeaflet.jsx
@@ -3,16 +3,15 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { OpenStreetMapTileLayer } from "./OpenStreetMapTileLayer";
 import { MarkerMap } from "./MarkerMap";
 
-export const MapaLeaflet = ({center, zoom}) => {
+export const MapaLeaflet = ({center, zoom, puntos}) => {
 
-    const ejemplo = [[28.117638,-115.157151], [31.736051,-116.294682], [30.985556,-119.044815]]
     return (
 
         <MapContainer center={center} zoom={zoom} >
 
         <OpenStreetMapTileLayer />
         {
-            ejemplo.map((coordenadas, index) => <MarkerMap position={coordenadas} text={index}/>)
+            puntos.map((coordenadas, index) => <MarkerMap position={coordenadas} text={index}/>)
         }
         
         </MapContainer>
--- app/albatross_trajectories/src/main.jsx
+++ app/albatross_trajectories/src/main.jsx
@@ -7,10 +7,11 @@ import { MapaTrayectorias } from './MapaTrayectorias.jsx'
 
 const guadalupeIsland = [28.883621, -118.292683]
 const defaultZoom = 9
+const ejemplo = [[28.117638,-115.157151], [31.736051,-116.294682], [30.985556,-119.044815]]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <MapaLeaflet center={guadalupeIsland} zoom={defaultZoom}/>
+  <MapaLeaflet center={guadalupeIsland} zoom={defaultZoom} puntos={ejemplo}/>
     {/*<MapaTrayectorias />*/}
   </React.StrictMode>,
 )
